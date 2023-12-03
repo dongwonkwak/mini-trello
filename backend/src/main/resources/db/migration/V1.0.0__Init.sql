fb132d3c335f61773a4f19ba43d0cfd61d5e4b87
dongwon.kwak <dongwon.kwak@gmail.com>
2023-12-03 17:18:08 +0900

    id 이름 entity 객체와 동일하게 변경. timestamp 추가

--- backend/src/main/resources/db/migration/V1.0.0__Init.sql
+++ backend/src/main/resources/db/migration/V1.0.0__Init.sql
@@ -1,8 +1,9 @@
 create schema if not exists minitrello;
 
 create TABLE IF NOT EXISTS minitrello.user_account(
-    id uuid NOT NULL DEFAULT random_uuid(),
+    user_id uuid NOT NULL DEFAULT random_uuid(),
     username varchar(20) NOT NULL,
     email varchar(24) NOT NULL,
-    password varchar(72) NOT NULL
+    password varchar(72) NOT NULL,
+    created_at TIMESTAMP NOT NULL
 )
