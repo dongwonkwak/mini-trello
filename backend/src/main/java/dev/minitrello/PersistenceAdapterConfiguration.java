2d76a815f1406dc9dfade7c4ae60000d9296f963
dongwon.kwak <dongwon.kwak@gmail.com>
2023-12-03 15:52:25 +0900

    UserAccountRepository 추가. UUID Converter 제거

---
+++ backend/src/main/java/dev/minitrello/PersistenceAdapterConfiguration.java
@@ -0,0 +1,2 @@
+package dev.minitrello;public class PersistenceAdapterConfiguration {
+}
---
+++ backend/src/main/java/dev/minitrello/adapters/output/h2/repository/UserAccountRepository.java
@@ -0,0 +1,2 @@
+package dev.minitrello.adapters.output.h2.repository;public interface UserAccountRepository {
+}
